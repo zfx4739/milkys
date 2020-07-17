6505f793d6a403bf33e9746b67e62166109b1fcd
zfx4739 <1198038482@qq.com>
2020-07-17 17:45:15 +0800

    添加对象元素 添加认证权限

---
+++ src/main/java/com/example/SecurityDemo/diary/a.java
@@ -0,0 +1,12 @@
+package com.example.SecurityDemo.diary;
+
+/**
+ * @author ：zfx
+ * @date ：Created in 2020/7/17 14:34
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+
+public class a {
+}
